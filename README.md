21d727ade1fe63112d25091ebe92efe1f0cbdab3
Sergi Martínez <sergi.martinez@gmail.com>
2017-10-07 20:07:34 +0200

    Lesson 1: Planets-Flutter: From design to app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# flutter_planets_tutorial
-The Flutter Planets app tutorial with commits per lesson
+# flutter planets tutorial
+
+This project contains the steps followed on the tutorial on sergiandreplace.com.
+
+Each lesson will correspond to a single branch with the name of the post where you'll be able to find the result of what is explained in the article
+
+Remember all the graphical assets and the design is created by Vijay Verma
+
+* Lesson 1: [Planets-Flutter: From design to app](http://sergiandreplace.com/2017/09/planets-flutter-from-design-to-app/)
+* Lesson 2: [Planets-Flutter: creating a Planet card](http://sergiandreplace.com/2017/09/planets-flutter-creating-a-planet-card/)
---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("treva"),
+        ],
+      ),
+    );
+  }
+}
+
+class GradientAppBar extends StatelessWidget {
+
+  final String title;
+  final double barHeight = 66.0;
+
+  GradientAppBar(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery
+      .of(context)
+      .padding
+      .top;
+
+    return new Container(
+      padding: new EdgeInsets.only(top: statusBarHeight),
+      height: statusBarHeight + barHeight,
+      child: new Center(
+        child: new Text(title,
+          style:const TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            fontSize: 36.0
+          ),
+        ),
+      ),
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [
+            const Color(0xFF3366FF),
+            const Color(0xFF00CCFF)
+          ],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp
+        ),
+      ),
+    );
+  }
+}
