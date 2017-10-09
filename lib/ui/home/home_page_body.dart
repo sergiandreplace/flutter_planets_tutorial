249af56e40f4b3e981ae8e4610d4acb8894f86d9
Sergi Martínez <sergi.martinez@gmail.com>
2017-10-09 20:04:43 +0200

    Awesome list with slivers

--- lib/ui/home/home_page_body.dart
+++ lib/ui/home/home_page_body.dart
@@ -7,9 +7,23 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Expanded(
-      child: new ListView.builder(
-        itemBuilder: (context, index) => new PlanetRow(planets[index]),
-        itemCount: planets.length,
+      child: new Container(
+        color: new Color(0xFF736AB7),
+        child: new CustomScrollView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: false,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              sliver: new SliverList(
+                delegate: new SliverChildBuilderDelegate(
+                    (context, index) => new PlanetRow(planets[index]),
+                  childCount: planets.length,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
