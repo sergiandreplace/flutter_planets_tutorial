628e0447bf269aa088a9483178bc124ff72427ed
Devon Carew <devoncarew@google.com>
2018-04-11 06:42:56 -0700

    fix an issue with overflow

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'home_page_body.dart';
 
 class HomePage extends StatelessWidget {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,10 +4,10 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import '../lib/main.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter_test/flutter_test.dart';
+//
+//import '../lib/main.dart';
 
 void main() {
 //  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
