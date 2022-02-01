3857efc4e0a37eb1a47312f30fd29a763e350681
chaudharydeepanshu <deepanshuc2141@outlook.com>
2022-02-01 13:51:09 +0530

    Updated project code according to flutter_lints recommendations.

--- lib/ui/home/home_page_body.dart
+++ lib/ui/home/home_page_body.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'planet_row.dart';
 
 class HomePageBody extends StatelessWidget {
+  const HomePageBody({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new PlanetRow();
+    return PlanetRow();
   }
 }
--- lib/ui/home/planet_row.dart
+++ lib/ui/home/planet_row.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 
 class PlanetRow extends StatelessWidget {
+  PlanetRow({Key? key}) : super(key: key);
 
-  final planetThumbnail = new Container(
-    margin: new EdgeInsets.symmetric(
-      vertical: 16.0
-    ),
+  final planetThumbnail = Container(
+    margin: const EdgeInsets.symmetric(vertical: 16.0),
     alignment: FractionalOffset.centerLeft,
-    child: new Image(
-      image: new AssetImage("assets/img/mars.png"),
+    child: const Image(
+      image: AssetImage("assets/img/mars.png"),
       height: 92.0,
       width: 92.0,
     ),
   );
 
-  final planetCard = new Container(
+  final planetCard = Container(
     height: 124.0,
-    margin: new EdgeInsets.only(left: 46.0),
-    decoration: new BoxDecoration(
-      color: new Color(0xFF333366),
+    margin: const EdgeInsets.only(left: 46.0),
+    decoration: BoxDecoration(
+      color: const Color(0xFF333366),
       shape: BoxShape.rectangle,
-      borderRadius: new BorderRadius.circular(8.0),
-      boxShadow: <BoxShadow>[
-        new BoxShadow(
+      borderRadius: BorderRadius.circular(8.0),
+      boxShadow: const <BoxShadow>[
+        BoxShadow(
           color: Colors.black12,
           blurRadius: 10.0,
-          offset: new Offset(0.0, 10.0),
+          offset: Offset(0.0, 10.0),
         ),
       ],
     ),
@@ -33,18 +32,17 @@ class PlanetRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      height: 120.0,
-      margin: const EdgeInsets.symmetric(
-        vertical: 16.0,
-        horizontal: 24.0,
-      ),
-      child: new Stack(
-        children: <Widget>[
-          planetCard,
-          planetThumbnail,
-        ],
-      )
-    );
+    return Container(
+        height: 120.0,
+        margin: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 24.0,
+        ),
+        child: Stack(
+          children: <Widget>[
+            planetCard,
+            planetThumbnail,
+          ],
+        ));
   }
 }
